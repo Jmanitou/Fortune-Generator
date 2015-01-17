69b36081dd1ab326a2327cee4fe00046111c7d64
doxbin <amayatandon@gmail.com>
2015-01-17 16:44:22 -0500

    fortunes.rb

--- models/fortunes.rb
+++ models/fortunes.rb
@@ -1,2 +1,13 @@
 require 'nokogiri'
 require 'open-uri'
+
+fortunesdoc = open("http://joshmadison.com/2008/04/20/fortune-cookie-fortunes/")
+
+fortunes = Nokogiri::HTML(fortunesdoc)
+
+#gives an array of all the fortunes
+fortunes.css(".col-md-12 ul li")
+
+#a specific fortune
+
+fortunes.css(".col-md-12 ul li")[10].children.text
